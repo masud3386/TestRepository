c475dbda282a01448ba8413f8e21e69ea117b371
masud3386 <masud3386@yahoo.com>
2023-03-04 11:49:54 -0500

    added one new class and made some changes to other class

---
+++ JavaFeb23/src/array/ArrayDemo.java
@@ -0,0 +1,33 @@
+package array;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		String[] s = new String[3];
+		
+		
+		s[0]= "Selenium";
+		s[1]= "Eclipse";
+		s[2]= "Java";
+		System.out.println(	s[1]);
+		
+		
+		for(int i=0;i<s.length;i++)
+		{
+			System.out.println(s[i]);
+		}
+		
+          int  [] num = {4,2,7};
+          
+          for(int i=0;i<num.length;i++)
+          {
+        	  System.out.println(num[i]);
+          }
+          System.out.println("example of enhance for loop");
+          for (int masud:num)
+          {
+        	  System.out.println(masud);
+          }
+	}
+
+}
---
+++ JavaFeb23/src/dataTypes/AddingNumbers.java
@@ -0,0 +1,20 @@
+package dataTypes;
+
+public class AddingNumbers {
+
+	public static void main(String[] args) {
+		int sum1= 0;
+		int i=0;
+		while(i<=15)
+		{
+			sum1=sum1+i;
+			i++;
+		}
+		
+		System.out.println("the resuly:" +sum1);
+		
+		
+		
+	}
+
+}
